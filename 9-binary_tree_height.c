7fdeab264adcac80ac1d4ecac9aa8946c056b43a
Boukind Rachid <boukind.r1@gmail.com>
2024-06-06 16:52:55 -0700

    a

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_t right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	size_t l = 0, r = 0;
-
-	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return ((l > r) ? l : r);
+		return (left_height > right_height) ? left_height : right_height;
+	}
+	return (0);
 }
