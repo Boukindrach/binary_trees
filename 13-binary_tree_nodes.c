50cd854a24635a1347add1bc78536f481998f267
Boukind Rachid <boukind.r1@gmail.com>
2024-06-06 17:00:10 -0700

    a

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
-{	
+{
 	if (tree)
 	{
 		size_t nodes = (tree->left || tree->right) ? 1 : 0;
+
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
-
 		return (nodes);
 	}
 	return (0);
